c68cb3b671bd71b5a39ffcb98ecf068e05c77b8b
Laurina Patnaik <laurinapatnaik20@gmail.com>
2025-06-25 00:54:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <h1>This code is from the devops class</h1>
+        <p>This is Laurina!</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
